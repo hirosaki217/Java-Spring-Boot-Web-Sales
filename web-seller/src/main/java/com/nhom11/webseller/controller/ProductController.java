0f6177331981857b71629a20e2d84d6d674980e4
hirosaki217 <hirosaki217@example.com>
2022-05-07 15:12:53 +0700

    add product

--- web-seller/src/main/java/com/nhom11/webseller/controller/ProductController.java
+++ web-seller/src/main/java/com/nhom11/webseller/controller/ProductController.java
@@ -1,34 +1,29 @@
 package com.nhom11.webseller.controller;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.nhom11.webseller.dto.ProductOptionRequest;
@@ -128,6 +123,7 @@ public class ProductController {
 	}
 
 	@GetMapping("/images/{filename:.+}")
+	@ResponseBody
 	public ResponseEntity<Resource> transferFile(String fileName) {
 		Resource file = storageService.loadAsResoure(fileName);
 		return ResponseEntity.ok()
